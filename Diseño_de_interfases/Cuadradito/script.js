46554ae48d15ef4f7b72e95d2b2ef611ea382a74
UmaraBaucia <ubaucialuci@escuelasproa.edu.ar>
2026-08-07 11:38:35 -0300

    corregi algo que me puso el auto completado y no me gusto

--- Diseño_de_interfases/Cuadradito/script.js
+++ Diseño_de_interfases/Cuadradito/script.js
@@ -1,11 +1,5 @@
-const cuadradito_de_colores = document.getElementById('cuadradito_de_colores');
+const cuadraditoDeColores = document.getElementById('cuadraditoDeColores');
 
 function cambiarColor(color) {
-  const mapaColores = {
-    rojo: '#ef4444',
-    verde: '#10b981',
-    azul: '#3b82f6'
-  };
-  const valor = mapaColores[color] || color;
-  cuadradito_de_colores.style.backgroundColor = valor;
+  cuadraditoDeColores.style.backgroundColor = color;
 }
